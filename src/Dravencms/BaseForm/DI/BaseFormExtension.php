7c165859cf63825210e6d3a90bb71a736f8b5264
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-14 18:30:40 +0200

    chore: Fix

---
+++ src/Dravencms/BaseForm/DI/BaseFormExtension.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Dravencms\BaseGrid\DI;
+
+use Nette\DI\CompilerExtension;
+
+/**
+ * Class BaseGridExtension
+ * @package Dravencms\BaseGrid\DI
+ */
+class BaseFormExtension extends CompilerExtension
+{
+    public function loadConfiguration(): void
+    {
+        $this->loadComponents();
+    }
+
+    protected function loadComponents(): void
+    {
+        $builder = $this->getContainerBuilder();
+        foreach ($this->loadFromFile(__DIR__ . '/components.neon') as $i => $command) {
+            $cli = $builder->addFactoryDefinition($this->prefix('components.' . $i));
+            if (is_string($command)) {
+                $cli->setImplement($command);
+            } else {
+                throw new \InvalidArgumentException;
+            }
+        }
+    }
+}
