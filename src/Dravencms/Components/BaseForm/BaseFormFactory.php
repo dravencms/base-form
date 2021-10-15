30cfac252767031a3de2e80e85842c185ce27834
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-15 18:39:24 +0200

    chore: Return correct form instance

--- src/Dravencms/Components/BaseForm/BaseFormFactory.php
+++ src/Dravencms/Components/BaseForm/BaseFormFactory.php
@@ -4,7 +4,7 @@ namespace Dravencms\Components\BaseForm;
 
 use Dravencms\Components\BaseControl\BaseControl;
 use Nette\Forms\FormRenderer;
-use Nette\Application\UI\Form;
+
 
 /**
  * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
